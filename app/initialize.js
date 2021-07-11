203b210338d6b1ca309b62a984f86cc5bf0a8577
Christopher R. Miller <xpm@mysteriouspants.com>
2021-07-10 17:56:37 -0700

    Clean up for deploy

--- app/initialize.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  var $ = require('jquery');
-  window.jQuery = $;
-  window.$ = $;
-  require('jquery-ujs');
-  console.log('Initialized app');
-});
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -o xtrace
 set -e
@@ -14,16 +14,14 @@ scp ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/db/app.sqlite $(pwd
 
 ssh ${SERVICE_SU_USER}@${SERVICE_HOST} sudo apt-get install libssl1.1 libsqlite3-0
 
-pushd cargo-project
-  # the main reason for containerizing the build is so that we can
-  # target the os the idevgames server runs rather than whatever your
-  # local machine happens to be running
-  docker run --rm --name idevgamesc \
-    -v $(pwd):/src \
-    -w /src \
-    rust:1.52 \
-    cargo build --release
-popd
+# the main reason for containerizing the build is so that we can
+# target the os the idevgames server runs rather than whatever your
+# local machine happens to be running
+docker run --rm --name idevgamesc \
+  -v $(pwd):/src \
+  -w /src \
+  rust:1.53 \
+  cargo build --release
 
 pushd react-project
   npm run-script build
@@ -32,7 +30,8 @@ popd
 # deploy the build if successful
 ssh ${SERVICE_SU_USER}@${SERVICE_HOST} sudo systemctl stop ${SERVICE_NAME}
 ssh ${SERVICE_USER_NAME}@${SERVICE_HOST} mkdir -p ${SERVICE_USER_DIR}/db
-rsync -avzr cargo-project/target/release/idevgames ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/idevgames
-rsync -avzr --delete react-project/build/static/ ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/static
+rsync -avzr target/release/idevgames ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/idevgames
+rsync -avzr --delete react-project/build/ ${SERVICE_USER_NAME}@${SERVICE_HOST}:${SERVICE_USER_DIR}/static/
 ssh ${SERVICE_USER_NAME}@${SERVICE_HOST} chmod +x ${SERVICE_USER_DIR}/idevgames
+ssh ${SERVICE_USER_NAME}@${SERVICE_HOST} ${SERVICE_USER_DIR}/idevgames migrate
 ssh ${SERVICE_SU_USER}@${SERVICE_HOST} sudo systemctl start ${SERVICE_NAME}
